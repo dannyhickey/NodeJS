352ac5b77a1dafcfd54d407ed48573e3525f71e1
Daniel Hickey <daniellhickey@gmail.com>
2016-05-17 17:18:52 +0100

    Adding an array with objects storing football stadium data

--- FirstNodeJSProject/app.js
+++ FirstNodeJSProject/app.js
@@ -1,10 +1,30 @@
 var express = require("express");
 var app = express();
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs");//allows node to render pages without needing to write ejs extension 
 
 app.get("/", function(req, res){
     res.render("index");
+});
+
+app.get("/footballGrounds", function(req, res) {
+    var footballGrounds = [
+        {
+            name: "Villa Park",
+            image: "https://upload.wikimedia.org/wikipedia/commons/b/b6/VillaPark_PanoramaFromTrinityRoadStand.jpg"
+        },
+        
+        {
+            name: "Anfield",
+            image: "https://upload.wikimedia.org/wikipedia/commons/8/86/Anfield,_20_October_2012.jpg"
+        },
+        
+        {
+            name: "The Emirates",
+            image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Emirates_Stadium_-_East_side_-_Composite.jpg"
+        }
+        ];
+        res.render("footballGrounds", {footballGrounds: footballGrounds});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
