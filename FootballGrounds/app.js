79ffa703eadfcfcd1c88f0fe800a8aa068fdf4d8
Daniel Hickey <daniellhickey@gmail.com>
2016-05-23 20:22:51 +0100

    Adding some more of the restful API functions and renaming index.ejs and footballGrounds.ejs to more conventional names

--- FootballGrounds/app.js
+++ FootballGrounds/app.js
@@ -7,74 +7,27 @@ mongoose.connect("mongodb://localhost/football_grounds");
 
 var footballGroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var FootballGround = mongoose.model("FootballGround", footballGroundSchema);
 
-/*FootballGround.create(
-        {
-            name: "Nowhere Park",
-            image: "https://upload.wikimedia.org/wikipedia/commons/0/01/Villa_Park.jpg"
-        }, function(err, grounds)
-        {
-            if(err){
-                console.log(err);
-            }else{
-                console.log("Created new footballGround");
-                console.log(grounds);
-            }
-        }
-    );*/
-
-/*  var footballGrounds =  
-    [
-        {
-            name: "Villa Park",
-            image: "https://upload.wikimedia.org/wikipedia/commons/0/01/Villa_Park.jpg"
-        },
-        
-        {
-            name: "Anfield",
-            image: "https://upload.wikimedia.org/wikipedia/commons/8/86/Anfield,_20_October_2012.jpg"
-        },
-        
-        {
-            name: "The Emirates",
-            image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Emirates_Stadium_-_East_side_-_Composite.jpg"
-        },
-        
-        {
-            name: "Villa Park",
-            image: "https://upload.wikimedia.org/wikipedia/commons/0/01/Villa_Park.jpg"
-        },
-        
-        {
-            name: "Anfield",
-            image: "https://upload.wikimedia.org/wikipedia/commons/8/86/Anfield,_20_October_2012.jpg"
-        },
-        
-        {
-            name: "The Emirates",
-            image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Emirates_Stadium_-_East_side_-_Composite.jpg"
-        },
-        
-        {
-            name: "Villa Park",
-            image: "https://upload.wikimedia.org/wikipedia/commons/0/01/Villa_Park.jpg"
-        },
-        
-        {
-            name: "Anfield",
-            image: "https://upload.wikimedia.org/wikipedia/commons/8/86/Anfield,_20_October_2012.jpg"
-        },
-        
-        {
-            name: "The Emirates",
-            image: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Emirates_Stadium_-_East_side_-_Composite.jpg"
-        }
-    ];*/
-
+// FootballGround.create(
+//         {
+//             name: "Villa Park",
+//             image: "https://upload.wikimedia.org/wikipedia/commons/0/01/Villa_Park.jpg",
+//             description: "This is the mighty villa park!"
+//         }, function(err, grounds)
+//         {
+//             if(err){
+//                 console.log(err);
+//             }else{
+//                 console.log("Created new footballGround");
+//                 console.log(grounds);
+//             }
+//         }
+//     );
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");//allows node to render pages without needing to write ejs extension 
@@ -83,13 +36,14 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
+//INDEX - shows all football grounds
 app.get("/footballGrounds", function(req, res){
     //Get all footballGrounds from DB
     FootballGround.find({}, function(err, grounds){
         if(err){
             console.log(err);
         }else{
-            res.render("footballGrounds", {footballGrounds: grounds});
+            res.render("index", {footballGrounds: grounds});
         }
     });
 });
@@ -109,6 +63,11 @@ app.post("/footballGrounds", function(req, res){
 
 app.get("/footballGrounds/new", function(req, res) {
    res.render("new.ejs"); 
+});
+
+//SHOW - shows more info about one football ground.
+app.get("/footballGrounds/:id", function(req, res) {
+   res.render("show");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
