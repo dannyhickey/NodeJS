262d59f9cd13e57efe2cb72bff7c26a1b9209161
Daniel Hickey <daniellhickey@gmail.com>
2016-05-30 16:59:31 +0100

    laying out and testing the index page

--- BlogApp_RESTful/app.js
+++ BlogApp_RESTful/app.js
@@ -2,19 +2,41 @@ var bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     express     = require("express"),
     app         = express();
-    
+
+//app config    
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs"); 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+//Mongoose model config
 var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
     created: {type: Date, default: Date.now}
 });
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("ERROR!");
+        }else{
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+
+
+// RESTful Routes
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
