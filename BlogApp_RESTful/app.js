6b2207dfc1b2d302d59644e71c950611bf508da1
Daniel Hickey <daniellhickey@gmail.com>
2016-05-30 14:38:44 +0100

    Creating the Schema for the blog

--- BlogApp_RESTful/app.js
+++ BlogApp_RESTful/app.js
@@ -7,10 +7,15 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs"); 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
-// title
-// image
-// body
-// date created
+
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Server has started!");
