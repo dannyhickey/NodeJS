2fb63751f9c267252d41b6ae07c66346b67a3a18
Daniel Hickey <daniellhickey@gmail.com>
2016-05-30 14:30:58 +0100

    Setting up

--- BlogApp_RESTful/app.js
+++ BlogApp_RESTful/app.js
@@ -4,7 +4,14 @@ var bodyParser  = require("body-parser"),
     app         = express();
     
 mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs"); 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 // title
 // image
 // body
-// created
+// date created
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The Server has started!");
+});
